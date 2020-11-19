975c638618572ba61d64960b963e65c22f4eeafe
mikolajp2137 <pienkoszmikolaj@gmail.com>
2020-11-19 03:07:06 +0100

    rrr

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-import math
-import os
-import sys
-
 import requests
 
-
-def greet(who):
-    greeting = "hello, {}".format(who)
-    return greeting
-
-
-print(greet('jarek'))
-print(sys.version)
 r = requests.get('https://google.com')
 print(r.status_code)
-a = 1
